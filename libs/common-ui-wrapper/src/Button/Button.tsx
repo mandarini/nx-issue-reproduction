b21c657ab842ee9f40eab55760d79acd4452405c
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-07-05 20:11:36 +0300

    use both button props

--- libs/common-ui-wrapper/src/Button/Button.tsx
+++ libs/common-ui-wrapper/src/Button/Button.tsx
@@ -1,6 +1,7 @@
-// button.component.tsx
-
 import React from 'react';
-import { Button as UiButton, ButtonProps } from '@myorg/common-ui';
+import { Button as UiButton } from '@myorg/common-ui';
+import type { ButtonProps as MuiButtonProps } from '@mui/material';
 
-export const Button = (props: ButtonProps) => <UiButton {...props} />;
+export const Button = (props: MuiButtonProps & { label: string }) => (
+  <UiButton {...props} />
+);
